6fee8bcaa979403e141c9e04ff2adf684c13eaca
ZloyLingvist <35521580+ZloyLingvist@users.noreply.github.com>
2020-04-30 14:35:24 +0500

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,7 @@
+echo Install TatSu
+pip install tatsu
+echo Install torch
+pip install --no-cache-dir torch==1.2.0+cpu torchvision==0.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html"
+echo Install stanfordnlp
+pip install stanfordnlp
+pause
---
+++ start.bat
@@ -0,0 +1,1 @@
+Script\gui.py

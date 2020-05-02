bb7dba8cdf82305bc6c4abf00ce2bd88d4f49a7a
ZloyLingvist <35521580+ZloyLingvist@users.noreply.github.com>
2020-05-02 14:24:40 +0500

    Add files via upload

--- install.bat
+++ install.bat
@@ -4,4 +4,6 @@ echo Install torch
 pip install --no-cache-dir torch==1.2.0+cpu torchvision==0.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html"
 echo Install stanfordnlp
 pip install stanfordnlp
+echo Install numpy
+pip install numpy
 pause
